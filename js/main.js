d55ea28509a260314036cd4a783b9f0068b0ba76
juliannascruggs <julianna@scrug.gs>
2014-10-22 14:26:39 -0500

    Set up a function to get the name and price from a list item when you click on an add cart button.

--- js/main.js
+++ js/main.js
@@ -9,11 +9,38 @@ $(document).ready(function(){
   }).fail(function() {
   	console.log( "Fail, noob." );
   });
-  // create a function for updating the cart total located at the bottom of `#cart` based on the items currently in the cart
+
+// create a function for updating the cart total located at the bottom of `#cart` based on the items currently in the cart
+
+function updateTotal(){
+  console.log('hai');
+};
 
   // attach an event listener to all `.buttons` in the `.list-item`s to detect clicks
     // when clicked, add the item to the table in `#cart` using the template in index.html
     // run the function you created to update the total
+
+$('.items').on('click', '.button', function(e){
+  e.preventDefault();
+
+  var itemName = $(this).parents('.list-item').find('.list-item-name').text();
+  console.log(itemName);
+
+  var itemPriceString = $(this).parents('.list-item').find('.list-item-price').text();
+  var itemPriceValue = itemPriceString.slice(1);
+  var itemPrice = parseInt(itemPriceValue);
+  console.log(itemPrice);
+
+$('#cart').append("<tr class='cart-item'><td class='cart-item-name'>" + itemName + "</td><td class='cart-item-price'>$" + itemPrice + "</td><td class='cart-item-remove'><a href='#' class='button'>Remove</a></td></tr>"
+);
+  // updateTotal();
+});
+
+
+
+// $('.price', this).val()
+
+
 
   // attach an event listener to all `.buttons` in the `.cart-item`s to detect clicks
     // when clicked, remove the item from the table in `#cart`
